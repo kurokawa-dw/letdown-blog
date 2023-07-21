f433b8515d8c1265608a3dcecc698f19ee177490
Keiichi Kurokawa <keiichi@kurokawa-dw.com>
2023-07-22 01:10:39 +0900

    typescriptちょっとやってみる

---
+++ src/lib/world.ts
@@ -0,0 +1,17 @@
+export default class World {
+	message: string;
+
+	constructor(message: string){
+		this.message = message;
+	}
+
+	public sayHello(elem: HTMLElement | null){
+		if(elem){
+			elem.innerText = this.message
+		}
+	}
+
+	public retMessage(){
+		return this.message
+	}
+}
