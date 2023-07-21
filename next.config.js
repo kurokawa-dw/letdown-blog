9512f6f53b7c999ddba55f94b866092b2b6acb32
Kurokawa <keiichi@kurokawa-dw.com>
2023-07-22 00:19:36 +0900

    ブログ作るよ

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+	images: {
+    domains: [
+    	process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+    ],
+  },
 }
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+			fontFamily: {
+				'body': ['Noto Sans JP', 'sans-serif'],
+				'accent': ['YuGothic', 'sans-serif']
+			}
     },
   },
-  plugins: [],
+  plugins: [
+		require('@tailwindcss/forms'),
+	],
 }
